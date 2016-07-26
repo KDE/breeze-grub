099a661ba6b6fd6e802f5f855ed8caa8fd692f59
Harald Sitter <sitter@kde.org>
2016-07-26 10:39:31 +0200

    redesign as per new proposal
    - all fonts changed to unifont
    - terminal font no longer overridden
    - shortcuts are now a vbox
    - color profile changed to b/w
    - highlight is now boldified rather than backgrounded
    
    this currently isn't compatible with heights < 768 due to bad maths on
    my part (actually just need to recalculate good base heights to make sure
    the shortcuts vbox reduces the overall height of the menu itself

---
+++ breeze/mkfont.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mk="grub-mkfont -v"
+
+input="/usr/share/fonts/truetype/unifont/unifont.ttf"
+$mk -s 14 -o unifont-regular-14.pf2 $input
+$mk -s 16 -o unifont-regular-16.pf2 $input
+$mk -s 16 -o unifont-bold-16.pf2 -b $input
+$mk -s 32 -o unifont-regular-32.pf2 $input
