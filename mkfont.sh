c4f4dc06a93194e1636d99742a0fffae486e61e2
Harald Sitter <sitter@kde.org>
2021-06-07 17:25:39 +0200

    add license files for unifont
    GPL2+ with font exception. the pf2 files are derived from ubuntu's
    unifont through the mkfont.sh script

--- mkfont.sh
+++ mkfont.sh
@@ -12,3 +12,5 @@ $mk -s 14 -o unifont-regular-14.pf2 $input
 $mk -s 16 -o unifont-regular-16.pf2 $input
 $mk -s 16 -o unifont-bold-16.pf2 -b $input
 $mk -s 32 -o unifont-regular-32.pf2 $input
+
+echo "MAKE SURE THE LICENSE HASN'T CHANGED!"
