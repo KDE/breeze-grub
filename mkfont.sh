8195a97d98de6d2d87d6937a44a222b0c596fc64
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-06-07 17:07:26 +0200

    Add license information in SPDX format

--- mkfont.sh
+++ mkfont.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+# SPDX-FileCopyrightText: 2016 Harald Sitter <sitter@kde.org>
+
 cd breeze
 
 mk="grub-mkfont -v"
