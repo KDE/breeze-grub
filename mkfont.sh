c66582fd14b5492d1b06803970fd218d6742bed9
Harald Sitter <sitter@kde.org>
2024-02-12 14:08:12 +0100

    mkfont: error on error

--- mkfont.sh
+++ mkfont.sh
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 # SPDX-FileCopyrightText: 2016 Harald Sitter <sitter@kde.org>
 
+set -e
+
 cd breeze
 
 # As per grub's makefile:
