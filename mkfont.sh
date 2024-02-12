eff9b2e68c769521fd3f220368d990de5a686b5d
Harald Sitter <sitter@kde.org>
2024-02-12 14:08:02 +0100

    fonts: optimize for ascii
    Considering we do not do localization of our UI strings we can get away
    with only ASCII characters. This reduces the theme size considerably

--- mkfont.sh
+++ mkfont.sh
@@ -5,12 +5,24 @@
 
 cd breeze
 
+# As per grub's makefile:
+# Arrows and lines are needed to draw the menu, so always include them
+UNICODE_ARROWS="0x2190-0x2193"
+UNICODE_LINES="0x2501-0x251B"
+ASCII_RANGE="0x0-0x7f,${UNICODE_ARROWS},${UNICODE_LINES}"
+
 mk="grub-mkfont -v"
 
 input="/usr/share/fonts/truetype/unifont/unifont.ttf"
-$mk -s 14 -o unifont-regular-14.pf2 $input
+
+# Since we don't have localization we only need ASCII here (0x0-0x7f)
+## The action description box font
+$mk -s 14 -o unifont-regular-14.pf2 --range "$ASCII_RANGE" $input
+## The title font
+$mk -s 32 -o unifont-regular-32.pf2 --range "$ASCII_RANGE" $input
+
+# The entry fonts are full unicode since we don't know what they will contain
 $mk -s 16 -o unifont-regular-16.pf2 $input
-$mk -s 16 -o unifont-bold-16.pf2 -b $input
-$mk -s 32 -o unifont-regular-32.pf2 $input
+$mk -s 16 -o unifont-bold-16.pf2 --bold $input
 
 echo "MAKE SURE THE LICENSE HASN'T CHANGED!"
